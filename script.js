dfb72267fe5acbc2ee8ee25aa65a20477f11fe77
Xeliozil <lukasz02k@gmail.com>
2024-11-17 15:46:11 +0100

    fixed
    every time you press a button "Pa tera" it lists every property of every object in array through a console.log

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function Silnia_I(n)
     }
     return wynik;
 }
+
+const users = [
+    {id: 1, name: "Jan", age: 25},
+    {id: 2, name: "Adam", age: 26},
+    {id: 3, name: "Grzegorz", age: 27}
+];
+function WylistowanieOsob(){
+    n=users.length;
+    i=0;
+    while (i<n){
+        console.log(users[i].id);
+        console.log(users[i].name);
+        console.log(users[i].age);
+        i++;
+    }
+}
