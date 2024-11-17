b72d89695562d3a4effaa455c0991d698050dec9
Xeliozil <lukasz02k@gmail.com>
2024-11-17 15:20:35 +0100

    added js file

---
+++ script.js
@@ -0,0 +1,19 @@
+function Hello(name)
+{
+    console.log("Hello, " + name);
+}
+
+function Suma(a, b)
+{
+    return a + b;
+}
+
+function Silnia_I(n)
+{
+    wynik = 1;
+    for (k = 1; k <= n; ++k)
+    {
+        wynik *= k;
+    }
+    return wynik;
+}
